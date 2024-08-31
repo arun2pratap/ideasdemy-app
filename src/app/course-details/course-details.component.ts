bab43632d47231db97ba7907d9bede45767fb122
Arun Pratap Singh <ArunPratap.Singh@iDeaS.com>
2024-09-01 02:11:24 +0530

    - course details added.

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.css']
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
+  course: any;
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit() {
+    const courseId = this.route.snapshot.params['id'];
+    this.http.get(`http://localhost:3000/courses/${courseId}`)
+      .subscribe((data: any) => {
+        this.course = data;
+      });
+  }
+
+  addToCart(course: any) {
+    let cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    cart.push(course);
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 }
