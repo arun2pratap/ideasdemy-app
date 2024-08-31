393adf185b8e5aacddd207273ab88dd5e7ebe79e
Arun Pratap Singh <ArunPratap.Singh@iDeaS.com>
2024-08-31 23:46:02 +0530

    - add components for 			ng generate component login 			ng generate component signup 			ng generate component home 			ng generate component course-details 			ng generate component cart-summary 			ng generate component checkout ng generate component order-complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderCompleteComponent } from './order-complete/order-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    CourseDetailsComponent,
+    CartSummaryComponent,
+    CheckoutComponent,
+    OrderCompleteComponent
   ],
   imports: [
     BrowserModule,
