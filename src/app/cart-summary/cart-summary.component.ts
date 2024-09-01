a02e0a71e1fd9bd886f623de8e3fd555a73f5d71
Arun Pratap Singh <ArunPratap.Singh@iDeaS.com>
2024-09-01 21:45:47 +0530

    - cart-summary component to read from localStorage

--- src/app/cart-summary/cart-summary.component.ts
+++ src/app/cart-summary/cart-summary.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart-summary',
   templateUrl: './cart-summary.component.html',
   styleUrls: ['./cart-summary.component.css']
 })
-export class CartSummaryComponent {
+export class CartSummaryComponent implements OnInit {
+  cart: any[] = [];
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  }
+
+  checkout() {
+    this.router.navigate(['/checkout']);
+  }
+
+  removeFromCart(courseId: number) {
+    this.cart = this.cart.filter(item => item.id !== courseId);
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
 }
