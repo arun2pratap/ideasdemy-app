bccefec9ea4f9bbf82ea17bfb5c932de3da368e4
Arun Pratap Singh <ArunPratap.Singh@iDeaS.com>
2024-09-01 22:10:20 +0530

    - adding navigation bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ideasdemy-app';
+export class AppComponent implements OnInit {
+  cartCount: number = 0;
+
+  ngOnInit() {
+    this.updateCartCount();
+  }
+
+  updateCartCount() {
+    const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    this.cartCount = cart.length;
+  }
 }
